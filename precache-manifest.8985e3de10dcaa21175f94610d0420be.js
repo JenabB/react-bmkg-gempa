171840ec7ad045245585a114171686b4586fda88
Yogi Surya Pranata <birulangit1996@gmail.com>
2020-11-15 00:52:44 +0700

    Updates

---
+++ precache-manifest.8985e3de10dcaa21175f94610d0420be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd2be21fed2d4f0a85299578a374274",
+    "url": "/react-bmkg-gempa/index.html"
+  },
+  {
+    "revision": "ae1ee346a911fef49b74",
+    "url": "/react-bmkg-gempa/static/css/main.6d19f8c8.chunk.css"
+  },
+  {
+    "revision": "8748ef60d01aa303301b",
+    "url": "/react-bmkg-gempa/static/js/2.444527f6.chunk.js"
+  },
+  {
+    "revision": "27ca4020711bd21574faae3da7dd14e9",
+    "url": "/react-bmkg-gempa/static/js/2.444527f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1ee346a911fef49b74",
+    "url": "/react-bmkg-gempa/static/js/main.38835e96.chunk.js"
+  },
+  {
+    "revision": "bb0b5e24f1e5875691b5",
+    "url": "/react-bmkg-gempa/static/js/runtime-main.9f24156a.js"
+  }
+]);
